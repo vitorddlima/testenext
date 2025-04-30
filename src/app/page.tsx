bfbdeee05fe0d148a3848a0043f7ef64f4e7d415
vitorddlima <vitordranka@gmail.com>
2025-04-30 16:42:19 -0300

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <main>
+        <h1 className="text-red-600">OIE AMO ESTUDAR ALOOH</h1>
+      </main>
+    </>
+  );
+}
